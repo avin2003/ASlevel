8f9b2fff86528776e7588373bd9cc0f841e1077e
avin2003 <69658737+avin2003@users.noreply.github.com>
2020-08-14 10:47:22 +0800

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,17 @@
+import turtle
+
+def triangle_draw(x,y,sz):
+    turtle.speed(0)
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    for i in range(3):
+        turtle.forward(sz)
+        turtle.left(120)
+
+
+for y in range (-100,100,20):
+    for x in range (-100,100,20):
+        triangle_draw(x,y,10)
+
+
